8629a297f16e2e96bda58bdbd8bc7750a2e6b36e
pachamaltese <mvargas@dcc.uchile.cl>
2018-12-13 21:03:05 +0000

    test with countries and products

--- api.R
+++ api.R
@@ -26,22 +26,61 @@ function(msg=""){
   list(msg = paste0("The message is: '", msg, "'"))
 }
 
-#* Echo back the input
+#* Echo back the result of a query on yrpc table
 #* @param y Year
 #* @param r Reporter ISO
 #* @param p Partner ISO
 #* @param l Commodity code length
 #* @get /yrpc
-function(y = 2015, r = "chl", p = "chn", l = NULL) {
-  query <- glue_sql("
-                SELECT *
-                FROM public.hs07_yrpc
-                WHERE year = {y}
-                AND reporter_iso = {r}
-                AND partner_iso = {p}
-                LIMIT 10
-                ", .con = con)
 
+function(y = 2015, r = "chl", p = "chn", l = NULL) {
+  if (is.null(l)) {
+    query <- glue_sql("
+                      SELECT *
+                      FROM public.hs07_yrpc
+                      WHERE year = {y}
+                      AND reporter_iso = {r}
+                      AND partner_iso = {p}
+                      ", .con = con)
+  } else {
+    query <- glue_sql("
+                      SELECT *
+                      FROM public.hs07_yrpc
+                      WHERE year = {y}
+                      AND reporter_iso = {r}
+                      AND partner_iso = {p}
+                      AND commodity_code_length = {l}
+                      ", .con = con)
+  }
+  
+  data <- dbGetQuery(con, query)
+  
+  toJSON(data)
+  }
+
+#* Echo back the result of a query on attributes_countries table
+#* @get /countries
+
+function() {
+  query <- glue_sql("
+                    SELECT *
+                    FROM public.attributes_country_names
+                    ", .con = con)
+  
+  data <- dbGetQuery(con, query)
+  
+  toJSON(data)
+}
+
+#* Echo back the result of a query on attributes_countries table
+#* @get /products
+
+function() {
+  query <- glue_sql("
+                    SELECT *
+                    FROM public.attributes_product_names
+                    ", .con = con)
+  
   data <- dbGetQuery(con, query)
   
   toJSON(data)
