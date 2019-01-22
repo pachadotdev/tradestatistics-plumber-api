3ab1c165b92f418ec548f4a38f55903630a105e3
pachamaltese <mvargas@dcc.uchile.cl>
2019-01-22 21:08:52 +0000

    adds yrc short table

--- queries.R
+++ queries.R
@@ -603,6 +603,116 @@ function(y = NULL, r = NULL, c = "all", l = 4) {
   return(data)
 }
 
+# YRC short (for Shiny) ---------------------------------------------------
+
+#* Echo back the result of a query on yrc table
+#* @param y Year
+#* @param r Reporter ISO
+#* @param c Commodity code
+#* @param l Commodity code length
+#* @get /yrc_short
+
+function(y = NULL, r = NULL, c = "all", l = 4) {
+  y <- as.integer(y)
+  r <- tolower(substr(as.character(r), 1, 4))
+  c <- tolower(substr(as.character(c), 1, 6))
+  l <- tolower(substr(as.character(l), 1, 3))
+  
+  if (nchar(c) == 2) { l <- "all" }
+  
+  if (nchar(y) != 4 | !y %in% 1962:2016) {
+    return("The specified year is not a valid integer value. Read the documentation: tradestatistics.github.io/documentation")
+    stop()
+  }
+  
+  if (!nchar(r) <= 4 | !r %in% c(countries_data$country_iso, continents_data$country_iso)) {
+    return("The specified reporter is not a valid ISO code or alias. Read the documentation: tradestatistics.github.io/documentation")
+    stop()
+  }
+  
+  if (!nchar(c) <= 6 | !c %in% c(products_data$commodity_code, groups_data$commodity_code)) {
+    return("The specified commodity is not a valid string. Read the documentation: tradestatistics.github.io/documentation")
+    stop()
+  }
+  
+  if (!nchar(l) <= 3 | !l %in% c(4, 6, "all")) {
+    return("The specified length is not a valid integer value. Read the documentation: tradestatistics.github.io/documentation")
+    stop()
+  }
+  
+  query <- glue_sql(
+    "
+    SELECT year, reporter_iso, commodity_code, export_value_usd, import_value_usd
+    FROM public.hs07_yrc
+    WHERE year = {y}
+    ", 
+    .con = con
+  )
+  
+  if (r != "all" & nchar(r) == 3) {  
+    query <- glue_sql(
+      query, 
+      " AND reporter_iso = {r}", 
+      .con = con
+    )
+  }
+  
+  if (r != "all" & nchar(r) == 4) {  
+    r2 <- switch(
+      r, 
+      "c-af" = countries_africa,
+      "c-am" = countries_americas,
+      "c-as" = countries_asia,
+      "c-eu" = countries_europe,
+      "c-oc" = countries_oceania
+    )
+    
+    query <- glue_sql(
+      query, 
+      " AND reporter_iso IN ({vals*})", 
+      vals = r2$country_iso,
+      .con = con
+    )
+  }
+  
+  if (c != "all" & nchar(c) != 2) {
+    query <- glue_sql(
+      query, 
+      " AND commodity_code = {c}",
+      .con = con
+    )
+  }
+  
+  if (c != "all" & nchar(c) == 2) {
+    query <- glue_sql(
+      query, 
+      " AND LEFT(commodity_code, 2) = {c}",
+      .con = con
+    )
+  }
+  
+  if (l != "all") {
+    query <- glue_sql(
+      query,
+      " AND commodity_code_length = {l}",
+      .con = con
+    )
+  }
+  
+  data <- dbGetQuery(con, query)
+  
+  if (nrow(data) == 0) {
+    data <- tibble(
+      year = y,
+      reporter_iso = r,
+      commodity_code = c,
+      observation = "No data available for these filtering parameters"
+    )
+  }
+  
+  return(data)
+}
+
 # YRP ---------------------------------------------------------------------
 
 #* Echo back the result of a query on yrp table
